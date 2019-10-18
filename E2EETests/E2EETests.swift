8ef66ed397800999c178f7cafe9cf71842c41534
Thang Nguyen <thangnvh@vng.com.vn>
2019-10-18 09:26:07 +0700

    Initial Commit

---
+++ E2EETests/E2EETests.swift
@@ -0,0 +1,34 @@
+//
+//  E2EETests.swift
+//  E2EETests
+//
+//  Created by Thang on 18/10/2019.
+//  Copyright © 2019 ThangNVH. All rights reserved.
+//
+
+import XCTest
+@testable import E2EE
+
+class E2EETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
