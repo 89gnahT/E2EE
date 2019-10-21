65f6f5304b6dfaaf249627de6a1d3902a988bb6d
HuynhLamPhuSi <1612559@student.hcmus.edu.vn>
2019-10-21 13:55:02 +0700

    Untested helper for iOS 13

--- E2EE/Helper/NativeHelper.swift
+++ E2EE/Helper/NativeHelper.swift
@@ -8,11 +8,13 @@
 
 import Foundation
 import CryptoKit
+import CommonCrypto
 
 protocol CryptoProtocol : NSObject {
-    func hashMethod(data : Data) throws -> Data // Example: SHA256, SHA512, ...
-    func hmacAuthentication(data : Data) throws -> Data // Example: hmacSHA256, hmacbcrypt, hmacMD5, ...
-    func crtypoAlgorithm(data : Data) -> Data //Example: AES, DES, IDEA, ...
+    func hashMethod(data : Data) throws -> Data? // Example: SHA256, SHA512, ...
+    func hmacAuthentication(data : Data, salt: Data) throws -> Data? // Example: hmacSHA256, hmacbcrypt, hmacMD5, ...
+    func encryptAlgorithm(data : Data, keyCipher: Data) throws -> Data? //Example: AES, DES, IDEA, ...
+    func decryptAlgorithm(data : Data, keyCipher: Data) throws -> Data? //Example: AES, DES, IDEA, ...
 }
 
 public class Crypto : NSObject {
@@ -21,26 +23,99 @@ public class Crypto : NSObject {
     }
 }
 
+//var context = CCHmacContext()
+//
+//let bytes = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+//withUnsafeMutablePointer(to: &context) { (ptr: UnsafeMutablePointer<CCHmacContext>) in
+//    // Pointer to salt
+//    salt.withUnsafeBytes { ptr2 in
+//        let saltPtr = ptr2.baseAddress!
+//        // Pointer to message
+//        message.withUnsafeBytes { ptr3 in
+//            let messagePtr = ptr3.baseAddress!
+//            // Authenticate
+//            CCHmacInit(ptr, CCHmacAlgorithm(kCCHmacAlgSHA256), saltPtr, salt.count)
+//            CCHmacUpdate(ptr, messagePtr, message.count)
+//            CCHmacFinal(ptr, UnsafeMutableRawPointer(mutating: bytes))
+//        }
+//    }
+//}
+//
+//return Data(bytes)
+
 extension Crypto : CryptoProtocol {
-    func hmacAuthentication(data: Data) throws -> Data {
-        <#code#>
+    
+    //HMAC using SHA512
+    func hmacAuthentication(data: Data, salt: Data) throws -> Data? {
+        if #available(iOS 13.0, *) {
+            let authenticationCode = HMAC<SHA512>.authenticationCode(for: data, using: SymmetricKey.init(data: salt))
+            return Data(authenticationCode)
+        } else {
+            var context = CCHmacContext()
+            let bytes = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+            withUnsafeMutablePointer(to: &context, {(ptr : UnsafeMutablePointer<CCHmacContext>) in
+                salt.withUnsafeBytes{ ptr2 in
+                    let saltPtr = ptr2.baseAddress!
+                    data.withUnsafeBytes({ptr3 in
+                        let dataPtr = ptr3.baseAddress!
+                        CCHmacInit(ptr, CCHmacAlgorithm(kCCHmacAlgSHA512), saltPtr, salt.count)
+                        CCHmacUpdate(ptr, dataPtr, data.count)
+                        CCHmacFinal(ptr, UnsafeMutableRawPointer(mutating: bytes))
+                    })
+                }
+            })
+            return Data(bytes)
+        }
     }
     
-    func crtypoAlgorithm(data: Data) -> Data {
-        <#code#>
+    //cryptoAlgorithm using AES
+    func encryptAlgorithm(data: Data, keyCipher: Data) throws -> Data? {
+        if #available(iOS 13.0, *) {
+            let encryptData = try! AES.GCM.seal(data, using: SymmetricKey(data: keyCipher), nonce: .none)
+            return encryptData.ciphertext
+        } else {
+            
+        }
+        return nil
+    }
+    
+    func decryptAlgorithm(data: Data, keyCipher: Data) throws -> Data? {
+        if #available(iOS 13.0, *) {
+            <#code#>
+        } else {
+            
+        }
     }
     
     
     //implement SHA512
-    func hashMethod(data: Data) throws -> Data {
+    func hashMethod(data: Data) throws -> Data? {
         if #available(iOS 13.0, *) {
             let hashData = SHA512.hash(data: data)
-            let hashValue = hashData.hashValue
-            
+            return Data(hashData)
         } else {
-            
+            var context = CC_SHA512_CTX()
+            return try withUnsafeMutablePointer(to: &context, {contextPtr in
+                CC_SHA512_Init(contextPtr)
+                let result: Int32 = data.withUnsafeBytes({ptr2 in
+                    let dataPtr = ptr2.baseAddress!
+                    return CC_SHA512_Update(contextPtr, dataPtr, CC_LONG(data.count))
+                })
+                guard result == 1 else {
+                    print("Error")
+                    return nil
+                }
+                var md = Data(count: Int(CC_SHA512_DIGEST_LENGTH))
+                let result2: Int32 = md.withUnsafeMutableBytes({ptr4 in
+                    let a = ptr4.baseAddress?.assumingMemoryBound(to: UInt8.self)
+                    return CC_SHA512_Final(a, contextPtr)
+                })
+                guard result2 == 1 else {
+                    print("Error")
+                    return nil
+                }
+                return md
+            })
         }
-        
-        
     }
 }
