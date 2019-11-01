3af3487338503ff1e908a6e2379e05be69191334
HuynhLamPhuSi <1612559@student.hcmus.edu.vn>
2019-11-01 14:33:07 +0700

    Untested Security

---
+++ E2EE/Security/DeviceConsistency/DeviceConsistencySignature.swift
@@ -0,0 +1,37 @@
+//
+//  DeviceConsistancySignature.swift
+//  E2EE
+//
+//  Created by CPU11899 on 11/1/19.
+//  Copyright © 2019 ThangNVH. All rights reserved.
+//
+
+import Foundation
+
+struct DeviceConsistencySignature {
+    var signature: Data
+    var vrfOutput: Data
+    
+    init(signature: Data, vrfOutput: Data) {
+        self.signature = signature
+        self.vrfOutput = vrfOutput
+    }
+}
+
+extension DeviceConsistencySignature: Comparable {
+    static func <(a: DeviceConsistencySignature, b: DeviceConsistencySignature) -> Bool {
+        guard a.vrfOutput.count == b.vrfOutput.count else {
+            return a.vrfOutput.count < b.vrfOutput.count
+        }
+        for i in 0..<a.vrfOutput.count {
+            if a.vrfOutput[i] != b.vrfOutput[i] {
+                return a.vrfOutput[i] < b.vrfOutput[i]
+            }
+        }
+        return false
+    }
+    
+    static func ==(a: DeviceConsistencySignature, b: DeviceConsistencySignature) -> Bool {
+        return a.vrfOutput == b.vrfOutput
+    }
+}
