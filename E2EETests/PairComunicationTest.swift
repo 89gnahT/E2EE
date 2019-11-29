05255369893ae9ee13b8f41a4db3c86acedb632f
Thang Nguyen <thangnvh@vng.com.vn>
2019-11-29 17:02:50 +0700

    Creating Unit Test: + cleaning up + simple implementation of creating session and receiving message in pair or in group

--- E2EETests/PairComunicationTest.swift
+++ E2EETests/PairComunicationTest.swift
@@ -9,6 +9,19 @@
 import XCTest
 @testable import E2EE
 
+class UnitTestUser {
+    var session: SessionCipher<SignalKeyStore>?
+    var address: SignalAddress?
+    var keyStore: SignalKeyStore?
+    var preKeyBundle: SessionPreKeyBundle?
+    
+    init(address: SignalAddress , keyStore: SignalKeyStore, preKeyBundle: SessionPreKeyBundle) {
+        self.address = address
+        self.keyStore = keyStore
+        self.preKeyBundle = preKeyBundle
+    }
+}
+
 class PairComunicationTest: XCTestCase {
     
     let crypto = CommonSignalCrypto()
@@ -24,93 +37,83 @@ class PairComunicationTest: XCTestCase {
     var bobAddress: SignalAddress?
     var bobSession: SessionCipher<SignalKeyStore>?
     
+    var testSignalModelArray: [SignalIdentityKeyStore]?
+    
     var message: CipherTextMessage?
-
+    
+    var array: [UnitTestUser?]?
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         self.socket = try! NetWorkSocket(withHost: "127.0.0.1", and: "8000")
+        array = [UnitTestUser]()
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testBobSignUp() {
-        self.bobAddress = SignalAddress(identifier: "02468", deviceId: 1)
-        
-        let identityKey = try? crypto.generateIdentityKeyPair()
-        guard let IKey = identityKey else {
-            XCTFail("Can't create key")
+    func testSignUp(identifier: String, deviceID: UInt32) {
+        let newAddress = SignalAddress(identifier: identifier, deviceId: deviceID)
+        let identityKey  = try? crypto.generateIdentityKeyPair()
+        guard let iKey = identityKey else {
+            XCTFail("Can't create identity key pair")
             return
         }
-        let keyStore = SignalKeyStore(withKeyPair: IKey)
-        self.bobKeyStore = keyStore
         
+        let newKeyStore = SignalKeyStore(withKeyPair: iKey)
+        
+        var prekeyBundle: SessionPreKeyBundle?
         do {
-            let publicKey = try keyStore.identityKeyStore.getPublicIdentityKey()
-            let preKeys = try keyStore.createPreKeys(count: 10)
-            let signedPreKey = try keyStore.updateSignedPrekey()
-            
-            self.bobPreKeyBundle = try SessionPreKeyBundle(preKey: preKeys.first!, signedPreKey: signedPreKey, identityKey: publicKey)
-            
-        } catch let error as SignalError {
+            let newPublicKey = try newKeyStore.identityKeyStore.getPublicIdentityKey()
+            let preKeys = try newKeyStore.createPreKeys(count: 10)
+            let signedPrekey = try newKeyStore.updateSignedPrekey()
+            prekeyBundle = try SessionPreKeyBundle(preKey: preKeys.first!, signedPreKey: signedPrekey, identityKey: newPublicKey)
+        }
+        catch let error as SignalError {
             print(error.description)
         } catch {
             print("Other error")
         }
+        
+        let newUser = UnitTestUser(address: newAddress, keyStore: newKeyStore, preKeyBundle: prekeyBundle!)
+        self.array?.append(newUser)
     }
-
-    func testAliceSignUp() {
-        self.aliceAddress = SignalAddress(identifier: "13579", deviceId: 2)
-        
-        let identityKey = try? crypto.generateIdentityKeyPair()
-        guard let IKey = identityKey else {
-            XCTFail("Can't create key")
-            return
-        }
-        let keyStore = SignalKeyStore(withKeyPair: IKey)
-        self.aliceKeyStore = keyStore
-        
+    
+    func testSendMessage(sender: UnitTestUser!, receiverAdd: SignalAddress!, message: Data) {
         do {
-            let publicKey = try keyStore.identityKeyStore.getPublicIdentityKey()
-            let preKeys = try keyStore.createPreKeys(count: 10)
-            let signedPreKey = try keyStore.updateSignedPrekey()
-            
-            self.alicePreKeyBundle = try SessionPreKeyBundle(preKey: preKeys.first!, signedPreKey: signedPreKey, identityKey: publicKey)
+            self.message = try sender.session!.encrypt(message)
+            //self.message = try session.encrypt(message)
         } catch let error as SignalError {
             print(error.description)
         } catch {
             print("Other error")
         }
-        
-        
-        //Send preKey bundle to server by socket
+        print("Sending ....")
+    }
+    
+    func testReceiveMessage(receiver: UnitTestUser!) {
+        do {
+            let data = try receiver.session?.decrypt(self.message!)
+            let message = String(decoding: data!, as: UTF8.self)
+            print("received: \(message)")
+        } catch let error as SignalError {
+            print(error.description)
+        } catch {
+            print("Other error")
+        }
     }
     
     func testAliceInitializeChatSession() {
-        testBobSignUp()
-        testAliceSignUp()
-        //Assume that Alice want to initialize chat session with Bob and downloaded Bob preKey bundle
-        guard let bobPreKeyBundle = self.bobPreKeyBundle, let _ = self.alicePreKeyBundle else {
-            XCTFail("Error with network or key has damage")
-            return
-        }
-        guard let bobAddress = self.bobAddress, let _ = self.aliceAddress, let aliceKeyStore = self.aliceKeyStore, let _ = self.bobKeyStore else {
-            XCTFail("Error with sign up process")
-            return
-        }
-        //let session = SessionCipher(store: aliceKeyStore, remoteAddress: bobAddress)
-        self.aliceSession = SessionCipher(store: aliceKeyStore, remoteAddress: bobAddress)
+        let alice = self.array![0]
+        let bob = self.array![1]
+        alice!.session = SessionCipher(store: alice!.keyStore!, remoteAddress: bob!.address!)
         do {
-            //try session.process(preKeyBundle: bobPreKeyBundle)
-            try self.aliceSession?.process(preKeyBundle: bobPreKeyBundle)
-            let initializeMessage = "Hello Bob, it's Alice".data(using: .utf8)
+            try alice!.session!.process(preKeyBundle: bob!.preKeyBundle!)
+            let initializeMessage = "Hello Bob, it's Alice. This is setup key message".data(using: .utf8)
             if let message = initializeMessage {
                 do {
-                    //self.message = try session.encrypt(message)
-                    self.message = try self.aliceSession?.encrypt(message)
-                    //let encryptedMessage = String(decoding: self.message!.data, as: UTF8.self)
-                    //print(encryptedMessage)
+                    self.message = try alice?.session!.encrypt(message)
                 } catch let error as SignalError {
                     print(error.description)
                 } catch {
@@ -122,29 +125,21 @@ class PairComunicationTest: XCTestCase {
         } catch {
             print("Other error")
         }
-        
     }
     
     func testBobSetupByRecieveAliceMessage() {
-        testBobSignUp()
-        testAliceSignUp()
-        testAliceInitializeChatSession()
-        //By accept making friend with Alice, Bob has Alice Address
-        guard let bobStore = self.bobKeyStore, let aliceAddress = self.aliceAddress else {
-            XCTFail("Sign up failed or didn't make friend")
-            return
-        }
-        self.bobSession = SessionCipher(store: bobStore, remoteAddress: aliceAddress)
+        let alice = self.array![0]
+        let bob = self.array![1]
+        bob!.session = SessionCipher(store: bob!.keyStore!, remoteAddress: alice!.address!)
         guard let cipherMessage = self.message else {
             XCTFail("Not receive message")
             return
         }
         
         do {
-            //let decryptedMessage = try session.decrypt(cipherMessage)
-            let decryptedMessage = try self.bobSession?.decrypt(cipherMessage)
-            let message = String(decoding: decryptedMessage!, as: UTF8.self)
-            print(message)
+            let decryptedMessage = try bob!.session!.decrypt(cipherMessage)
+            let message = String(decoding: decryptedMessage, as: UTF8.self)
+            print("Bob receive \(message)")
         } catch let error as SignalError {
             print(error.longDescription)
         } catch {
@@ -152,108 +147,38 @@ class PairComunicationTest: XCTestCase {
         }
     }
     
-    //Bob send message
-    func sendBobMessage(message: Data) {
-        guard let bobStore = self.bobKeyStore, let aliceAddress = self.aliceAddress else {
-            XCTFail("Sign up failed or didn't making friend")
-            return
-        }
-        do {
-            self.message = try self.bobSession?.encrypt(message)
-            //self.message = try session.encrypt(message)
-        } catch let error as SignalError {
-            print(error.description)
-        } catch {
-            print("Other error")
-        }
-        
-        //Send message to server
-    }
-    
-    //Bob receive message
-    func receiveAliceMessage() {
-        guard let bobStore = self.bobKeyStore, let aliceAddress = self.aliceAddress else {
-            XCTFail("Sign up failed or didn't making friend")
-            return
-        }
-        do {
-            let data = try self.bobSession?.decrypt(self.message!)
-            //let data = try session.decrypt(self.message!)
-            let message = String(decoding: data!, as: UTF8.self)
-            print("Bob received: \(message)")
-        } catch let error as SignalError {
-            print(error.description)
-        } catch {
-            print("Other error")
-        }
-    }
-    
-    //Alice send message
-    func sendAliceMessage(message: Data) {
-        guard let aliceStore = self.aliceKeyStore, let bobAddress = self.bobAddress else {
-            XCTFail("Sign up failed or did'nt making friend")
-            return
-        }
-        do {
-            //self.message = try session.encrypt(message)
-            self.message = try self.aliceSession?.encrypt(message)
-        } catch let error as SignalError {
-            print(error.description)
-        } catch {
-            print("Other error")
-        }
-    }
-    
-    //Alice received message
-    func receiveBobMessage()  {
-        guard let aliceStore = self.aliceKeyStore, let bobAddress = self.bobAddress else {
-            XCTFail("Sign up failed or didn't making friend")
-            return
-        }
-        do {
-            let data = try self.aliceSession?.decrypt(self.message!)
-            //let data = try session.decrypt(self.message!)
-            let message = String(decoding: data!, as: UTF8.self)
-            print("Alice receive: \(message)")
-        } catch let error as SignalError {
-            print(error.description)
-        } catch {
-            print("Other error")
-        }
-    }
-    
-    func testBobAndAliceComunication() {
-        testBobSignUp()
-        testAliceSignUp()
+    func testDriver() {
+        testSignUp(identifier: "AliceIdentifier", deviceID: 1)
+        testSignUp(identifier: "BobIdentifier", deviceID: 2)
         testAliceInitializeChatSession()
         testBobSetupByRecieveAliceMessage()
+        let bob = array![1]
+        let alice = array![0]
         
-        let bobMessage : [String] = ["Hello you", "Are you ready?", "Hello world", "Can you love me?", "Old But Gold"]
         let aliceMessage : [String] = ["Oh hi Bob", "Yeah", "Welcome home", "My heart are broken", "Long time no see"]
-//        for i in 0..<5 {
-//            var data = bobMessage[i].data(using: .utf8)
-//            sendBobMessage(message: data!)
-//            receiveBobMessage()
-//            data = aliceMessage[i].data(using: .utf8)
-//            sendAliceMessage(message: data!)
-//            receiveAliceMessage()
-//        }
+        let bobMessage : [String] = ["Hello you", "Are you ready?", "Hello world", "Can you love me?", "Old But Gold"]
         
-        var data = bobMessage[0].data(using: .utf8)
-        sendBobMessage(message: data!)
-        data = bobMessage[1].data(using: .utf8)
-        sendBobMessage(message: data!)
-        receiveBobMessage()
+        for i in 0..<5 {
+            var data = bobMessage[i].data(using: .utf8)
+            testSendMessage(sender: bob, receiverAdd: alice?.address, message: data!)
+            testReceiveMessage(receiver: alice)
+            data = aliceMessage[i].data(using: .utf8)
+            testSendMessage(sender: alice, receiverAdd: bob?.address, message: data!)
+            testReceiveMessage(receiver: bob)
+        }
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
+
+
+
 
 extension PairComunicationTest: NetbaseSocketDelegate {
     func receive(_ data: Data) {
@@ -274,8 +199,6 @@ extension PairComunicationTest: NetbaseSocketDelegate {
             print("Connect is setting up")
         case .waiting:
             print("Connect is waiting")
-        default:
-            print("No thing happen")
         }
     }
     
